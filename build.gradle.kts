84edec900cbe183691a66b47aace0e970fb3ee90
Gonçalo Silva <goncalossilva@gmail.com>
2021-11-09 23:22:35 +0000

    Install git hooks automatically using Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,3 +63,11 @@ subprojects {
         }
     }
 }
+
+// Install git hooks automatically.
+gradle.taskGraph.whenReady {
+    val from = File("${rootProject.rootDir}/config/detekt/pre-commit")
+    val to = File("${rootProject.rootDir}/.git/hooks/pre-commit")
+    from.copyTo(to, overwrite = true)
+    to.setExecutable(true)
+}
