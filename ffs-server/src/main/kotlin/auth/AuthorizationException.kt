e0e8e5d981d868938dd7082729feebbb7fadb51c
Gonçalo Silva <goncalossilva@gmail.com>
2022-01-11 03:28:48 +0000

    Fix inverted authorize condition

--- ffs-server/src/main/kotlin/auth/AuthorizationException.kt
+++ ffs-server/src/main/kotlin/auth/AuthorizationException.kt
@@ -3,7 +3,7 @@ package doist.ffs.auth
 class AuthorizationException : RuntimeException()
 
 fun authorize(condition: Boolean?) {
-    if (condition == true) {
+    if (condition != true) {
         throw AuthorizationException()
     }
 }
