a0bb115bf2e79a8dbd041ec7d34c34b309c31404
Gonçalo Silva <goncalossilva@gmail.com>
2022-01-14 00:54:03 +0000

    Fix visibility warnings in clients

--- ffs-shared/client-library/src/commonMain/kotlin/Config.kt
+++ ffs-shared/client-library/src/commonMain/kotlin/Config.kt
@@ -3,27 +3,27 @@ package doist.ffs
 import kotlinx.serialization.json.JsonObject
 
 @Suppress("TooManyFunctions")
-public interface Config {
-    public val apiToken: String
+interface Config {
+    val apiToken: String
 
-    public val url: String
-    public val liveUpdates: Boolean
+    val url: String
+    val liveUpdates: Boolean
 
-    public val env: JsonObject
+    val env: JsonObject
 
-    public fun setRolloutId(rolloutId: String): Config
+    fun setRolloutId(rolloutId: String): Config
 
-    public fun setUserId(id: String): Config
-    public fun setUserEmail(email: String): Config
+    fun setUserId(id: String): Config
+    fun setUserEmail(email: String): Config
 
-    public fun setDeviceName(name: String): Config
-    public fun setDeviceLocale(locale: String): Config
+    fun setDeviceName(name: String): Config
+    fun setDeviceLocale(locale: String): Config
 
-    public fun putString(key: String, value: String): Config
-    public fun putNumber(key: String, value: Number): Config
-    public fun putBoolean(key: String, value: Boolean): Config
+    fun putString(key: String, value: String): Config
+    fun putNumber(key: String, value: Number): Config
+    fun putBoolean(key: String, value: Boolean): Config
 
-    public fun putListString(key: String, values: List<String>): Config
-    public fun putListNumber(key: String, values: List<Number>): Config
-    public fun putListBoolean(key: String, values: List<Boolean>): Config
+    fun putListString(key: String, values: List<String>): Config
+    fun putListNumber(key: String, values: List<Number>): Config
+    fun putListBoolean(key: String, values: List<Boolean>): Config
 }
