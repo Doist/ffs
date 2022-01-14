b67e35a94ac28c1dd7c67dac25e1c1e0d20098a4
Gonçalo Silva <goncalossilva@gmail.com>
2022-01-14 00:54:32 +0000

    Order shared submodules alphabetically

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,9 +8,9 @@ include(":ffs-library-backend")
 include(":ffs-dashboard")
 
 include(":ffs-shared")
+include(":ffs-shared:client-library")
 include(":ffs-shared:env")
 include(":ffs-shared:rule")
 include(":ffs-shared:sse")
-include(":ffs-shared:client-library")
 
 enableFeaturePreview("VERSION_CATALOGS")
