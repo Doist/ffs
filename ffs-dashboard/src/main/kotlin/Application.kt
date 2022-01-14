c635309c28bf7efcc3d8842b35734dccc612a50c
Gonçalo Silva <goncalossilva@gmail.com>
2022-01-14 02:28:28 +0000

    Remove placeholder dashboard code
    It's broken and unused.

--- ffs-dashboard/src/main/kotlin/Application.kt
+++ ffs-dashboard/src/main/kotlin/Application.kt
@@ -1,13 +1,5 @@
 package doist.ffs
 
-import kotlinx.browser.document
-import react.dom.h1
-import react.dom.render
-
 fun main() {
-    render(document.getElementById("root")!!) {
-        h1 {
-            +"Hello, world!"
-        }
-    }
+    // TBD.
 }
