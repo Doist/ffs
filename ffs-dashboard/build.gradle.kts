ef0fba1ce3095516c51294df37a29ca0b25cc613
Gonçalo Silva <goncalossilva@gmail.com>
2022-01-17 22:56:32 +0000

    Setup base dashboard dependencies

--- ffs-dashboard/build.gradle.kts
+++ ffs-dashboard/build.gradle.kts
@@ -1,9 +1,20 @@
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
+
 plugins {
     id(libs.plugins.kotlin.js.get().pluginId)
+    id(libs.plugins.kotlin.serialization.get().pluginId)
 }
 
 dependencies {
+    implementation(project(":ffs-shared:endpoints"))
+    implementation(project(":ffs-shared:validators"))
+
+    implementation(libs.bundles.client.ktor)
     implementation(libs.bundles.client.react)
+
+    implementation(npm("@doist/reactist", "^11.0.0"))
+    // Peer dependency of Reactist.
+    implementation(npm("classnames", "^2.2.5"))
 
     testImplementation(libs.kotlin.test)
 }
